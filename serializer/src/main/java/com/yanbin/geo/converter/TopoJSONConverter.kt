7e8e86f4e23875b4c90fe44792fdde2b6d82d1b7
yanbinHung <hung_yanbin@hotmail.com>
2021-04-17 18:12:38 +0800

    Implement MultiPolygon in TopoJSONConverter.kt

--- serializer/src/main/java/com/yanbin/geo/converter/TopoJSONConverter.kt
+++ serializer/src/main/java/com/yanbin/geo/converter/TopoJSONConverter.kt
@@ -3,7 +3,7 @@ package com.yanbin.geo.converter
 import com.yanbin.geo.core.Geometry
 import com.yanbin.geo.core.PointF
 import com.yanbin.geo.core.PolygonF
-import com.yanbin.geo.serializer.Arc
+import com.yanbin.geo.serializer.ArcIndex
 import com.yanbin.geo.serializer.ArcIndexList
 import com.yanbin.geo.serializer.ArcIntIndex
 import com.yanbin.geo.serializer.GeoObject
@@ -11,6 +11,7 @@ import com.yanbin.geo.serializer.GeoType
 import com.yanbin.geo.serializer.MultiArc
 import com.yanbin.geo.serializer.PositionArc
 import com.yanbin.geo.serializer.TopoJSON
+import com.yanbin.geo.serializer.Transform
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 
@@ -28,18 +29,82 @@ class TopoJSONConverter {
                convertGeoObject(it, topoJSON)
            }
            GeoType.Polygon -> listOf(convertPolygon(geoObject, topoJSON))
+           GeoType.MultiPolygon -> listOf(convertMultiPolygon(geoObject, topoJSON))
             else -> listOf()
         }
     }
 
-    private fun convertPolygon(geoObject: GeoObject, topoJSON: TopoJSON): Geometry {
-        val indexes = (geoObject.arcIndex as ArcIndexList).indexes[0].let { innerArcIndex ->
+    private fun convertMultiPolygon(geoObject: GeoObject, topoJSON: TopoJSON): Geometry {
+        val polygonIndexes = (geoObject.arcIndex as ArcIndexList).indexes
+
+        val polygons = polygonIndexes
+            .map { arcIndex ->
+                findAllPolygonArcIndexes(arcIndex)
+            }.map { polygonArcIndexes ->
+                constructSinglePolygon(polygonArcIndexes, topoJSON)
+            }
+
+        return Geometry(
+            polygons = polygons,
+            propertyList = emptyList()
+        )
+    }
+
+    private fun findAllPolygonArcIndexes(arcIndex: ArcIndex): List<Int> {
+        return (arcIndex as ArcIndexList).indexes[0].let { innerArcIndex ->
             (innerArcIndex as ArcIntIndex).indexes
         }
+    }
 
-        require(indexes.size == 1)
-        val index = indexes[0]
+    private fun convertPolygon(geoObject: GeoObject, topoJSON: TopoJSON): Geometry {
+        val polygonArcIndexes = findAllPolygonArcIndexes(geoObject.arcIndex!!)
+        val polygon = constructSinglePolygon(polygonArcIndexes, topoJSON)
 
+        return Geometry(
+            polygons = listOf(polygon),
+            propertyList = emptyList()
+        )
+    }
+
+    private fun constructSinglePolygon(
+        arcIndexes: List<Int>,
+        topoJSON: TopoJSON
+    ): PolygonF {
+        val pathInAbsolutePoints = arcIndexes.flatMapIndexed { index: Int, arcIndex: Int ->
+            val arc = constructArcFromIndex(topoJSON, arcIndex)
+            if (index != 0) {
+                arc.drop(1)
+            } else {
+                arc
+            }
+        }
+
+        val polygon = PolygonF(
+            pathInAbsolutePoints
+                .applyTransform(topoJSON.transform)
+                .toList()
+        )
+        return polygon
+    }
+
+    private fun List<PointF>.applyTransform(transform: Transform?): List<PointF> {
+        val scaleX = transform?.scale?.first ?: 1f
+        val scaleY = transform?.scale?.second ?: 1f
+        val offsetX = transform?.translate?.first ?: 0f
+        val offsetY = transform?.translate?.second ?: 0f
+
+        return this.map { normalizedPoint ->
+            PointF(
+                normalizedPoint.x * scaleX + offsetX,
+                normalizedPoint.y * scaleY + offsetY
+            )
+        }
+    }
+
+    private fun constructArcFromIndex(
+        topoJSON: TopoJSON,
+        index: Int
+    ): Sequence<PointF> {
         val pathInArc = (topoJSON.arcs as MultiArc).arcs[index] as MultiArc
         val pathInDeltaPoints = pathInArc.arcs
             .asSequence()
@@ -48,31 +113,11 @@ class TopoJSONConverter {
                 PointF(position.x.toFloat(), position.y.toFloat())
             }
 
-        val pathInAbsolutePoints = pathInDeltaPoints.scan(PointF.DEFAULT) { accPoint, delta ->
+        return pathInDeltaPoints.scan(PointF.DEFAULT) { accPoint, delta ->
             val endX = accPoint.x + delta.x
             val endY = accPoint.y + delta.y
 
             PointF(endX, endY)
         }.drop(1)
-
-        val scaleX = topoJSON.transform?.scale?.first ?: 1f
-        val scaleY = topoJSON.transform?.scale?.second ?: 1f
-        val offsetX = topoJSON.transform?.translate?.first ?: 0f
-        val offsetY = topoJSON.transform?.translate?.second ?: 0f
-
-        val transformedPoints = pathInAbsolutePoints
-            .map { normalizedPoint ->
-                PointF(
-                    normalizedPoint.x * scaleX + offsetX,
-                    normalizedPoint.y * scaleY + offsetY
-                )
-            }
-
-        val polygon = PolygonF(transformedPoints.toList())
-
-        return Geometry(
-            polygons = listOf(polygon),
-            propertyList = emptyList()
-        )
     }
 }
--- serializer/src/test/kotlin/com/yanbin/geo/converter/TopoJSONConverterTest.kt
+++ serializer/src/test/kotlin/com/yanbin/geo/converter/TopoJSONConverterTest.kt
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test
 internal class TopoJSONConverterTest {
 
     @Test
-    internal fun convertOnePolygon() {
+    internal fun `convert One Polygon`() {
         val data = """
         {
           "type": "Topology",
@@ -54,6 +54,55 @@ internal class TopoJSONConverterTest {
 
         assertEquals(expectGeometry, geometry)
     }
+
+    @Test
+    internal fun `convert One Polygon with two arc indexs`() {
+        val data = """
+        {
+          "type": "Topology",
+          "objects": {
+            "example": {
+              "type": "GeometryCollection",
+              "geometries": [
+                {   
+                  "type": "Polygon",
+                  "arcs": [[0,1]]
+                }
+              ]
+            }
+          },
+          "arcs": [
+            [[0, 0], [0, 9999], [2000, 0]],
+            [[2000, 9999], [0, -9999], [-2000, 0]]
+          ]
+        }
+    """.trimIndent()
+
+        val expectGeometry = Geometry(
+            polygons = listOf(
+                PolygonF(
+                    contour = listOf(
+                        PointF(0f, 0f),
+                        PointF(0f, 9999f),
+                        PointF(2000f, 9999f),
+                        PointF(2000f, 0f),
+                        PointF(0f, 0f),
+                    )
+                )
+            ),
+            propertyList = emptyList()
+        )
+
+        val converter = TopoJSONConverter()
+        val geometries = converter.fromString(data)
+
+        assertTrue(geometries.size == 1)
+        val geometry = geometries[0]
+
+        assertEquals(expectGeometry, geometry)
+    }
+
+//    [[[9]],[[10,11,12,13,14,15]],[[16]]]
 
     @Test
     internal fun `convert One Polygon with transform`() {
@@ -105,4 +154,126 @@ internal class TopoJSONConverterTest {
 
         assertEquals(expectGeometry, geometry)
     }
+
+    @Test
+    internal fun `convert One MultiPolygon with transform`() {
+        val data = """
+        {
+          "type": "Topology",
+          "transform": {
+              "scale": [0.0005, 0.0001],
+              "translate": [0, 0]
+          },
+          "objects": {
+            "example": {
+              "type": "GeometryCollection",
+              "geometries": [
+                {   
+                  "type": "MultiPolygon",
+                  "arcs": [[[0]],[[1]]]
+                }
+              ]
+            }
+          },
+          "arcs": [
+            [[0, 0], [0, 9999], [2000, 0], [0, -9999], [-2000, 0]],
+            [[2000, 10000], [0, 9999], [2000, 0], [0, -9999], [-2000, 0]]
+          ]
+        }
+    """.trimIndent()
+
+        val expectGeometry = Geometry(
+            polygons = listOf(
+                PolygonF(
+                    contour = listOf(
+                        PointF(0f, 0f),
+                        PointF(0f, 0.9999f),
+                        PointF(1f, 0.9999f),
+                        PointF(1f, 0f),
+                        PointF(0f, 0f),
+                    )
+                ),
+                PolygonF(
+                    contour = listOf(
+                        PointF(1f, 1f),
+                        PointF(1f, 1.9999f),
+                        PointF(2f, 1.9999f),
+                        PointF(2f, 1f),
+                        PointF(1f, 1f),
+                    )
+                )
+            ),
+            propertyList = emptyList()
+        )
+
+        val converter = TopoJSONConverter()
+        val geometries = converter.fromString(data)
+
+        assertTrue(geometries.size == 1)
+        val geometry = geometries[0]
+
+        assertEquals(expectGeometry, geometry)
+    }
+
+    @Test
+    internal fun `convert One MultiPolygon with transform and many arc indexes`() {
+        val data = """
+        {
+          "type": "Topology",
+          "transform": {
+              "scale": [0.0005, 0.0001],
+              "translate": [0, 0]
+          },
+          "objects": {
+            "example": {
+              "type": "GeometryCollection",
+              "geometries": [
+                {   
+                  "type": "MultiPolygon",
+                  "arcs": [[[0,1]],[[2]]]
+                }
+              ]
+            }
+          },
+          "arcs": [
+            [[0, 0], [0, 9999], [2000, 0]],
+            [[2000, 9999], [0, -9999], [-2000, 0]],
+            [[2000, 10000], [0, 9999], [2000, 0], [0, -9999], [-2000, 0]]
+          ]
+        }
+    """.trimIndent()
+
+        val expectGeometry = Geometry(
+            polygons = listOf(
+                PolygonF(
+                    contour = listOf(
+                        PointF(0f, 0f),
+                        PointF(0f, 0.9999f),
+                        PointF(1f, 0.9999f),
+                        PointF(1f, 0f),
+                        PointF(0f, 0f),
+                    )
+                ),
+                PolygonF(
+                    contour = listOf(
+                        PointF(1f, 1f),
+                        PointF(1f, 1.9999f),
+                        PointF(2f, 1.9999f),
+                        PointF(2f, 1f),
+                        PointF(1f, 1f),
+                    )
+                )
+            ),
+            propertyList = emptyList()
+        )
+
+        val converter = TopoJSONConverter()
+        val geometries = converter.fromString(data)
+
+        assertTrue(geometries.size == 1)
+        val geometry = geometries[0]
+
+        assertEquals(expectGeometry, geometry)
+    }
+
 }
